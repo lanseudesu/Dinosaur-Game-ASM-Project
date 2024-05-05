a33945e069479da64492be99bf1285133825d790
lanseudesu <lantisfernandez29@gmail.com>
2024-05-06 02:17:32 +0800

    clean code a bit

--- boulder.asm
+++ boulder.asm
@@ -4,28 +4,27 @@
 .code
 
 main PROC
-    mov ah, 0          	; 0=Set Video mode (AL=Mode)
-    mov al, 13h        	; mode 13 (VGA 320x200 256 color)
-    int 10h            	; bios int
+    mov ah, 0          	
+    mov al, 13h        	
+    int 10h            	
 	
-    ; Set background color to white
-    mov ax, 0A000h      ; Segment of video memory
+    mov ax, 0A000h      
     mov es, ax
-    xor di, di          ; Starting offset of video memory
-    mov cx, 320*200     ; Total number of pixels (320 * 200)
+    xor di, di          
+    mov cx, 320*200    
 
-    mov al, 0Fh         ; White color
-    rep stosb           ; Set all pixels to white
+    mov al, 0Fh         
+    rep stosb           
 
-    mov dx, 400         ; x-coordinate greater than the width of the screen
+    mov dx, 400         
     mov dh, dl
-    mov dl, 22          ; y-coordinate
+    mov dl, 22          
 
-    call ShowSprite     ; Draw the sprite
+    call ShowSprite    
 
     infloop:
     gravity:
-        cmp dh, 50       ; Check if the sprite has moved out of the screen on the left side
+        cmp dh, 50       
         jge onGround
         call ShowSprite   
         dec dh
@@ -40,7 +39,7 @@ main PROC
         cmp al, 'w'
         je moveUp
         
-        jmp infloop     ; cont loop if w is not pressed
+        jmp infloop     
 
 moveUp:
     mov ecx, 4
@@ -56,86 +55,82 @@ main ENDP
 
 
 Delay PROC
-    push cx             ; save original value of cx
+    push cx             
     mov ecx, 50000   
     delay_loop:
-        nop             ; no operation
+        nop             
         loop delay_loop
     pop cx
     ret
 Delay ENDP
 
 ReadChar PROC
-    mov ah, 01h         ; check for key press w/o waiting
+    mov ah, 01h         
     int 16h            
-    jz @F               ; jmp to end if z = set (no key pressed)
+    jz @F               
     mov ah, 00h         
     int 16h 
 @@:    
     ret
 ReadChar ENDP
 
-; XOR sprite at (X,Y) pos (DH,DL)
+
 ShowSprite PROC
     push cx
-    ; calculate screen pos
-    mov ax, @code       ; point DS to this segment
+    mov ax, @code       
     mov ds, ax
     
-    ;prepares the 8x8 space for sprite
-    push dx             ; preserve dx
-    mov ax, 15          ; 8 bytes per 8x8 block
+    push dx             
+    mov ax, 15          
     mul dh
     mov di, ax
-    mov ax, 15*320      ; 320 bytes per line, 8 lines per block
+    mov ax, 15*320     
     mov bx, 0
     add bl, dl
     mul bx
     add di, ax
-    pop dx              ; pop back dx, ES:DI is VRAM destination
+    pop dx              
     
     ; draw XOR sprite  
-    mov si, offset BitmapTest    ; si=Source bitmap
+    mov si, offset BitmapTest    
     mov cl, 15                  ; height
 
-; iterates through each row
 DrawBitmap_Yagain:              
     push di
     mov ch, 15                  ; width
 
-; iterates through each column
 DrawBitmap_Xagain:              
     mov al, [SI]
-    xor al, byte ptr es:[di]   ; XOR with current screen data.
-    mov byte ptr es:[di], al   ; store the result back into memory
+    xor al, byte ptr es:[di]  
+    mov byte ptr es:[di], al  
     inc si
     inc di
     dec ch
-    jnz DrawBitmap_Xagain       ; next column of pixels
+    jnz DrawBitmap_Xagain      
     pop di
-    add di, 320                 ; move down 1 line (320 pixels)
+    add di, 320                 
     inc bl
     dec cl
-    jnz DrawBitmap_Yagain       ; next row of pixels
+    jnz DrawBitmap_Yagain       
     pop cx
     ret 
 ShowSprite ENDP
 
-BitmapTest:             ; dinosaur, 1 byte per pixel
-    DB 00h,00h,00h,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,00h,00h    ;  0
-    DB 00h,0Fh,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,05h,0Fh,00h    ;  1
-    DB 0Fh,05h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,05h,0Fh    ;  2
-    DB 0Fh,05h,0Fh,0Dh,00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,0Fh    ;  3
-    DB 00h,0Fh,0Fh,0Dh,00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,0Fh    ;  4
-    DB 0Fh,05h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,0Dh,0Dh,0Fh,0Fh    ;  5
-    DB 0Fh,05h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,0Fh    ;  6
-    DB 00h,0Fh,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,0Fh    ;  7
-    DB 00h,00h,00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,0Fh,0Fh,00h    ;  8
-    DB 00h,00h,00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,00h,00h,00h    ;  9
-    DB 00h,0Fh,0Fh,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,00h,00h,00h    ;  10
-    DB 00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,00h,00h,00h    ;  11
-    DB 00h,0Fh,0Fh,0Dh,0Dh,0Dh,0Fh,0Fh,0Fh,0Dh,0Dh,0Fh,00h,00h,00h    ;  12
-    DB 00h,00h,0Fh,0Fh,0Fh,0Dh,0Fh,00h,0Fh,0Dh,0Fh,00h,00h,00h,00h    ;  13
-    DB 00h,00h,00h,00h,0Fh,0Fh,0Fh,00h,0Fh,0Fh,0Fh,00h,00h,00h,00h    ;  14
+BitmapTest:             
+    DB 00h,00h,00h,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,00h,00h    
+    DB 00h,0Fh,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,05h,0Fh,00h   
+    DB 0Fh,05h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,05h,0Fh    
+    DB 0Fh,05h,0Fh,0Dh,00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,0Fh    
+    DB 00h,0Fh,0Fh,0Dh,00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,0Fh   
+    DB 0Fh,05h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,0Dh,0Dh,0Fh,0Fh    
+    DB 0Fh,05h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,0Fh    
+    DB 00h,0Fh,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,05h,0Fh    
+    DB 00h,00h,00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,0Fh,0Fh,00h    
+    DB 00h,00h,00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,00h,00h,00h    
+    DB 00h,0Fh,0Fh,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,00h,00h,00h    
+    DB 00h,0Fh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Dh,0Fh,00h,00h,00h    
+    DB 00h,0Fh,0Fh,0Dh,0Dh,0Dh,0Fh,0Fh,0Fh,0Dh,0Dh,0Fh,00h,00h,00h   
+    DB 00h,00h,0Fh,0Fh,0Fh,0Dh,0Fh,00h,0Fh,0Dh,0Fh,00h,00h,00h,00h    
+    DB 00h,00h,00h,00h,0Fh,0Fh,0Fh,00h,0Fh,0Fh,0Fh,00h,00h,00h,00h    
 
 END main
--- ground.asm
+++ ground.asm
@@ -9,26 +9,25 @@ BoulderY db 0
 .code
 
 main PROC
-    mov ax, @code       ; point DS to this segment
+    mov ax, @code       
     mov ds, ax
-    mov ah, 0           ; 0=Set Video mode (AL=Mode)
-    mov al, 13h         ; mode 13 (VGA 320x200 256 color)
-    int 10h             ; bios int
-    
-    ; Set background color to white
-    mov ax, 0A000h      ; Segment of video memory
+    mov ah, 0           
+    mov al, 13h        
+    int 10h             
+
+    mov ax, 0A000h      
     mov es, ax
-    xor di, di          ; Starting offset of video memory
-    mov cx, 320*170     ; Total number of pixels (320 * 200)
+    xor di, di         
+    mov cx, 320*170     
 
-    mov al, 0Bh         ; White color
-    rep stosb           ; Set all pixels to white
+    mov al, 0Bh         
+    rep stosb           
     
-    mov cx, 320*30     ; Total number of pixels (320 * 200)
+    mov cx, 320*30     
 
-    mov al, 02h         ; White color
+    mov al, 02h         
     rep stosb   
-    ; Draw the sprite at its initial position
+    
     mov DinoX, 0
     mov DinoY, 23
     call ShowDino
@@ -65,19 +64,19 @@ moveUp:
 main ENDP
 
 Delay PROC
-    push cx             ; save original value of cx
+    push cx            
     mov ecx, 50000   
     delay_loop:
-        nop             ; no operation
+        nop             
         loop delay_loop
     pop cx
     ret
 Delay ENDP
 
 ReadChar PROC
-    mov ah, 09H         ; check for key press w/o waiting
+    mov ah, 09H         
     int 16h            
-    jz @F               ; jmp to end if z = set (no key pressed)
+    jz @F               
     mov ah, 00h         
     int 16h 
 @@:    
@@ -85,31 +84,31 @@ ReadChar PROC
 ReadChar ENDP
 
 ShowDino PROC
-    mov dh, DinoX      ; Fetch Dino's X position
-    mov dl, DinoY      ; Fetch Dino's Y position
+    mov dh, DinoX      
+    mov dl, DinoY      
     call ShowSprite
     ret
 ShowDino ENDP
 
 showBoulder PROC
-    mov dh, BoulderX      ; Fetch Dino's X position
-    mov dl, BoulderY      ; Fetch Dino's Y position
+    mov dh, BoulderX      
+    mov dl, BoulderY      
     call ShowSprite
     ret
 showBoulder ENDP
 
-; XOR sprite at (X,Y) pos (DH,DL)
+
 ShowSprite PROC
     mov ax, @code       ; point DS to this segment
     mov ds, ax
     push cx
     
-    ;prepares the 8x8 space for sprite
+    
     push dx             ; preserve dx
-    mov ax, 15          ; 8 bytes per 8x8 block
+    mov ax, 15          
     mul dh
     mov di, ax
-    mov ax, 15*320      ; 320 bytes per line, 8 lines per block
+    mov ax, 15*320      
     mov bx, 0
     add bl, dl
     mul bx
@@ -117,47 +116,45 @@ ShowSprite PROC
     pop dx              ; pop back dx, ES:DI is VRAM destination
     
     ; draw XOR sprite  
-    mov si, offset BitmapTest    ; si=Source bitmap
+    mov si, offset BitmapTest    
     mov cl, 15                  ; height
 
-; iterates through each row
 DrawBitmap_Yagain:              
     push di
     mov ch, 15                  ; width
 
-; iterates through each column
 DrawBitmap_Xagain:              
     mov al, [SI]
-    xor al, byte ptr es:[di]   ; XOR with current screen data.
-    mov byte ptr es:[di], al   ; store the result back into memory
+    xor al, byte ptr es:[di]   
+    mov byte ptr es:[di], al   
     inc si
     inc di
     dec ch
-    jnz DrawBitmap_Xagain       ; next column of pixels
+    jnz DrawBitmap_Xagain       
     pop di
-    add di, 320                 ; move down 1 line (320 pixels)
+    add di, 320                 
     inc bl
     dec cl
-    jnz DrawBitmap_Yagain       ; next row of pixels
+    jnz DrawBitmap_Yagain       
     pop cx
     ret 
 ShowSprite ENDP
 
-BitmapTest:             ; dinosaur, 1 byte per pixel
-    DB 00h,00h,00h,0Bh,0Bh,0Bh,0Bh,0BH,0BH,0BH,0BH,0BH,0BH,00h,00h    ;  0
-    DB 00h,0BH,0BH,09H,09H,09H,09H,09H,09H,09H,09H,01H,01H,0BH,00h    ;  1
-    DB 0BH,01H,0BH,09H,09H,09H,09H,09H,09H,09H,09H,09H,01H,01H,0BH    ;  2
-    DB 0BH,01H,0BH,09H,04h,0BH,09H,09H,09H,09H,09H,09H,09H,01H,0BH    ;  3
-    DB 00h,0BH,0BH,09H,04h,0BH,09H,09H,09H,09H,09H,09H,09H,01H,0BH    ;  4
-    DB 0BH,01H,0BH,09H,09H,09H,09H,09H,09H,09H,0BH,09H,09H,0BH,0BH    ;  5
-    DB 0BH,01H,0BH,09H,09H,09H,09H,09H,09H,09H,09H,09H,09H,01H,0BH    ;  6
-    DB 00h,0BH,0BH,09H,09H,09H,09H,09H,09H,09H,09H,09H,09H,01H,0BH    ;  7
-    DB 00h,00h,00h,0BH,09H,09H,09H,09H,09H,09H,09H,0BH,0BH,0BH,00h    ;  8
-    DB 00h,00h,00h,0BH,09H,09H,09H,09H,09H,09H,09H,0BH,00h,00h,00h    ;  9
-    DB 00h,0BH,0BH,0BH,09H,09H,09H,09H,09H,09H,09H,0BH,00h,00h,00h    ;  10
-    DB 00h,0BH,09H,09H,09H,09H,09H,09H,09H,09H,09H,0BH,00h,00h,00h    ;  11
-    DB 00h,0BH,0BH,09H,09H,09H,0BH,0BH,0BH,09H,09H,0BH,00h,00h,00h    ;  12
-    DB 00h,00h,0BH,0BH,0BH,09H,0BH,00h,0BH,09H,0BH,00h,00h,00h,00h    ;  13
-    DB 00h,00h,00h,00h,0BH,0BH,0BH,00h,0BH,0BH,0BH,00h,00h,00h,00h    ;  14
+BitmapTest:             
+    DB 00h,00h,00h,0Bh,0Bh,0Bh,0Bh,0BH,0BH,0BH,0BH,0BH,0BH,00h,00h   
+    DB 00h,0BH,0BH,09H,09H,09H,09H,09H,09H,09H,09H,01H,01H,0BH,00h    
+    DB 0BH,01H,0BH,09H,09H,09H,09H,09H,09H,09H,09H,09H,01H,01H,0BH    
+    DB 0BH,01H,0BH,09H,04h,0BH,09H,09H,09H,09H,09H,09H,09H,01H,0BH   
+    DB 00h,0BH,0BH,09H,04h,0BH,09H,09H,09H,09H,09H,09H,09H,01H,0BH    
+    DB 0BH,01H,0BH,09H,09H,09H,09H,09H,09H,09H,0BH,09H,09H,0BH,0BH  
+    DB 0BH,01H,0BH,09H,09H,09H,09H,09H,09H,09H,09H,09H,09H,01H,0BH   
+    DB 00h,0BH,0BH,09H,09H,09H,09H,09H,09H,09H,09H,09H,09H,01H,0BH  
+    DB 00h,00h,00h,0BH,09H,09H,09H,09H,09H,09H,09H,0BH,0BH,0BH,00h  
+    DB 00h,00h,00h,0BH,09H,09H,09H,09H,09H,09H,09H,0BH,00h,00h,00h    
+    DB 00h,0BH,0BH,0BH,09H,09H,09H,09H,09H,09H,09H,0BH,00h,00h,00h    
+    DB 00h,0BH,09H,09H,09H,09H,09H,09H,09H,09H,09H,0BH,00h,00h,00h    
+    DB 00h,0BH,0BH,09H,09H,09H,0BH,0BH,0BH,09H,09H,0BH,00h,00h,00h    
+    DB 00h,00h,0BH,0BH,0BH,09H,0BH,00h,0BH,09H,0BH,00h,00h,00h,00h    
+    DB 00h,00h,00h,00h,0BH,0BH,0BH,00h,0BH,0BH,0BH,00h,00h,00h,00h    
 
 END main
