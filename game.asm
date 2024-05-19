1f8086dcd9b62e8dd5d95fb6756ebb8fa6d2abfd
lanseudesu <lantisfernandez29@gmail.com>
2024-05-19 13:17:11 +0800

    done game over screen

--- game.asm
+++ game.asm
@@ -9,6 +9,7 @@
     include sprite.inc       ; sprite related procs
     include fifteen.inc      ; 15x15 sprites
     include alphabet.inc     ; 10x10 letters
+    include score.inc        ; score printing
 
     DinoXY dw 960 dup (?)    ; dh = x, dl = y
     curDinoXY dw 0           ; cur = current
@@ -28,7 +29,47 @@
 
     thousands db 0
     newThousands db 0
+
+    xloc dw 0
+    yloc dw 0
+    wid dw 0
+    height dw 0
+    color db 0
 .code
+
+draw macro x, y, w, h, c         
+	mov xloc, x
+	mov yloc, y
+	mov wid, w
+	mov height, h
+    mov color, c
+	call addRec
+endm
+
+addRec proc
+	mov cx, xloc              
+	mov dx, yloc
+	drawLoop:
+		mov ah, 0Ch           ;draw pixel
+		mov al, color         ;color
+		mov bh, 00h           ;page number, always 0
+		int 10h
+
+		inc cx
+		mov ax, cx
+		sub ax, xloc
+		cmp ax, wid
+		jng drawLoop
+		
+		mov cx, xloc
+		inc dx
+		
+		mov ax, dx
+		sub ax, yloc
+		cmp ax, height
+		jng drawLoop
+	ret
+addRec endp
 
 main PROC
     mov ax, @code       
@@ -168,9 +209,111 @@ main PROC
             cmp al, 1
             je l3  ; jumping
             jmp l4 ; falling
-
-
 main ENDP
+
+deadcls proc
+    mov ax, 0A000h      
+    mov es, ax
+    ; cyan bg color
+    xor di, di         
+    mov cx, 320*180     
+
+    mov al, 0Bh         
+    rep stosb   
+
+    mov cx, 320*20     
+    mov al, 02h         ; grass
+    rep stosb   
+
+    ;draw bracket y
+    draw 128, 98, 2, 1, 2ah
+    draw 128, 99, 1, 10, 2ah
+    draw 128, 110, 2, 1, 2ah
+    draw 133, 99, 2, 4, 2ah
+    draw 139, 99, 2, 4, 2ah
+    draw 134, 103, 2, 1, 2ah
+    draw 138, 103, 2, 1, 2ah
+    draw 135, 104, 4, 1, 2ah
+    draw 136, 105, 2, 4, 2ah
+    draw 144, 98, 2, 1, 2ah
+    draw 145, 99, 1, 10, 2ah
+    draw 144, 110, 2, 1, 2ah
+    ;draw bracket x
+    draw 128, 118, 2, 1, 04h
+    draw 128, 119, 1, 10, 04h
+    draw 128, 130, 2, 1, 04h
+    draw 133, 119, 2, 3, 04h
+    draw 139, 119, 2, 3, 04h
+    draw 134, 122, 2, 1, 04h
+    draw 138, 122, 2, 1, 04h
+    draw 136, 123, 2, 1, 04h
+    draw 134, 125, 2, 1, 04h
+    draw 138, 125, 2, 1, 04h
+    draw 133, 126, 2, 4, 04h
+    draw 139, 126, 2, 4, 04h
+    draw 144, 118, 2, 1, 04h
+    draw 145, 119, 1, 10, 04h
+    draw 144, 130, 2, 1, 04h
+
+    mov dx, 010bh  
+    mov al, 1       ; flag for dead dino sprite
+    call drawDino   ; draw dead dino sprite
+    call tryAgainScreen
+    call ReadChar
+
+    promptLoop:
+        cmp al, 'y'
+        je cls
+        cmp al, 'x'
+        je exit
+        jmp promptLoop
+    
+    exit:
+        mov ah, 4CH
+        int 21h
+deadcls endp
+
+cls proc                ; restart game
+    mov ones, 0
+    mov tens, 0
+    mov hundreds, 0
+    mov thousands, 0
+
+    mov ax, @code       
+    mov ds, ax
+
+    mov ax, 0A000h      
+    mov es, ax
+    ; cyan bg color
+    xor di, di         
+    mov cx, 320*180     
+
+    mov al, 0Bh         
+    rep stosb   
+
+    mov cx, 320*20     
+    mov al, 02h         ; grass
+    rep stosb   
+    
+    mov ax, @data
+    mov ds, ax 
+
+    mov dx, 010bh
+    call drawDino 
+    mov curDinoXY, dx
+    call drawOnes
+    mov dx, 1c00h
+    lea si, num0
+    call printSmallLetter
+    mov dx, 1b00h
+    lea si, num0
+    call printSmallLetter
+    mov dx, 1a00h
+    lea si, num0
+    call printSmallLetter
+
+    jmp infloop
+cls endp
 
 checkCollision PROC       
     push bx
@@ -206,6 +349,7 @@ checkCollision PROC
             inc dh
         loop readcharacter
         call printScore
+        call deadcls
         mov ah, 4CH
         int 21h
 checkCollision ENDP
@@ -227,11 +371,6 @@ printScore proc
     int 21h
     ret
 printScore endp
-
-addNine proc
-    add dl, 9
-    ret
-addNine endp
 
 delayy PROC
     push cx            
